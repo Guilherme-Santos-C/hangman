807dd9cbb9d37d28026a0b45d4df0ed987889ab8
Guilherme-Santos-C <guilherme44.santos.costa@gmail.com>
2024-01-21 23:23:57 -0300

    som funcionando

--- src/typescript/main.ts
+++ src/typescript/main.ts
@@ -1,1 +1,19 @@
-import { toca_musicas, para_musicas } from "./modules/audio";
+import { autoplay_musicas, retoma_musicas, para_musicas } from "./modules/audio";
+
+let icone_som = document.querySelector(".volume_icon") as HTMLImageElement;
+let tocando_musica = true;
+
+icone_som.addEventListener("click", () => {
+    if(tocando_musica){
+        para_musicas()
+        tocando_musica = false;
+        icone_som.src = "./images/volumeOf_icon.png"
+    }
+    else{
+        retoma_musicas()
+        tocando_musica = true;
+        icone_som.src = "./images/volumeOn_icon.png"
+    }
+})
+
+autoplay_musicas()
--- src/typescript/modules/audio.ts
+++ src/typescript/modules/audio.ts
@@ -1,28 +1,29 @@
-export {toca_musicas, para_musicas};
+export {autoplay_musicas, para_musicas, retoma_musicas};
 import { shuffleArray } from "./embaralhaArrays";
 
 let index = 0;
 
 const musicas_game:Array<HTMLAudioElement> = shuffleArray([
-    new Audio('../../../public/music/game_music1.mp3'),
-    new Audio('../../../public/music/game_music2.mp3'), 
-    new Audio('../../../public/music/game_music3.mp3')
+    new Audio('./music/game_music1.mp3'),
+    new Audio('./music/game_music2.mp3'), 
+    new Audio('./music/game_music3.mp3')
     ]) 
 
-const toca_musicas = () => {
-    musicas_game[index].play()
-    musicas_game[index].addEventListener("ended" , () => {
-        toca_musicas()
-    })
-    index += 1;
-    if(index == 3){
+const autoplay_musicas = () => {
+    if(index >= musicas_game.length){
         index = 0
     }
-}
+    musicas_game[index].play()
+    musicas_game[index].addEventListener("ended" , () => {
+        index += 1;
+        autoplay_musicas()
+        })
+    }
 
 const para_musicas = () => {
-    console.log("chamou")
-    musicas_game[1-index].pause()
+    musicas_game[index].pause()
 }
 
-
+const retoma_musicas = () => {
+    musicas_game[index].play()
+}
