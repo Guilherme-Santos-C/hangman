6b60e259b57517965eeb4e2c8a77dd50a572190c
Guilherme-Santos-C <guilherme44.santos.costa@gmail.com>
2024-07-25 13:17:39 -0300

    teste responsividade

--- src/typescript/main.ts
+++ src/typescript/main.ts
@@ -1,6 +1,6 @@
 import { autoplay_musicas, retoma_musicas, para_musicas } from "./modules/audio";
 
-let icones_som = [document.querySelector(".volume_icon") as HTMLImageElement, document.querySelector(".volume_icon_mobile") as HTMLImageElement];
+let icones_som:HTMLImageElement[] = [document.querySelector(".volume_icon")!, document.querySelector(".volume_icon_mobile")!];
 let tocando_musica = true;
 
 icones_som.forEach((iconeDeSom, index) => {
@@ -24,3 +24,32 @@ autoplay_musicas()
 
 // Aplicação da lógica do funcionamento das músicas através das funções importadas,
 // somente uma vez através do forEach, para as duas mediaQuerys o icone(anchor Element) do mobile esta sendo tratado como imagem para facilitar implementação  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
